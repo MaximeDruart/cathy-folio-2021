05d2ef5b4a7f49391f3c0492656cf6708a5649e7
Maxime Druart <mdruart@area17.com>
2021-09-29 13:00:15 +0200

    adding template component

---
+++ src/components/shared/BlockText.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import styled from "styled-components"
+
+const StyledBlockText = styled.div``
+
+const BlockText = () => {
+  return <div></div>
+}
+
+export default BlockText
