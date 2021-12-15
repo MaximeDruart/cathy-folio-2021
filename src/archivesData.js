fd1f65e7f8562ff0643dc9fa3212b16c1f79f979
CathyDolle <cathy.dolle@live.fr>
2021-12-15 15:34:51 +0100

    archives compress

--- src/archivesData.js
+++ src/archivesData.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid"
 
 import cl from "./assets/img/archives/cl.jpg"
 import cl_print from "./assets/img/archives/cl_print.jpg"
-import shibuya_print from "./assets/img/archives/shibuya_print.jpg"
+import katy_v4 from "./assets/img/archives/katy_v4.jpg"
 import shibuya_device from "./assets/img/archives/shibuya_device.jpg"
 import mandala from "./assets/img/archives/mandala.jpg"
 import mandarin from "./assets/img/archives/mandarin.jpg"
@@ -13,14 +13,58 @@ import abidaisy from "./assets/img/archives/abidaisy.jpg"
 import designr from "./assets/img/archives/designr.jpg"
 import etude from "./assets/img/archives/etude.jpg"
 import nike from "./assets/img/archives/nike.jpg"
-import illu1 from "./assets/img/archives/illu1.png"
-import illu2 from "./assets/img/archives/illu2.png"
-import vans from "./assets/img/archives/vans.png"
-import aker from "./assets/img/archives/aker.png"
-import minecraft from "./assets/img/archives/minecraft.png"
+import illu1 from "./assets/img/archives/illu1.jpg"
+import illu2 from "./assets/img/archives/illu2.jpg"
+import vans from "./assets/img/archives/vans.jpg"
+import aker from "./assets/img/archives/aker.jpg"
+import minecraft from "./assets/img/archives/minecraft.jpg"
+import akali from "./assets/img/archives/akali.jpg"
+import kill from "./assets/img/archives/kill.jpg"
+import etudePhone from "./assets/img/archives/etude_phone.jpg"
+import shibuCard from "./assets/img/archives/shibuCard.jpg"
 
 
 let archivesData = [
+  {
+    name: "DesignR",
+    path: "DesignR",
+    coverImg: designr,
+    type: "website",
+    description: "An API for designR (work only on desktop)",
+    websiteLink: "https://designr.cathydolle.com/",
+  },
+  {
+    name: "Etude house",
+    path: "Etude house",
+    coverImg: etude,
+    type: "Ui Design",
+    description: "UI Design for Étude House",
+    websiteLink: "",
+  },
+    {
+    name: "Kill geometry print",
+    path: "Kill geometry print",
+    coverImg: kill,
+    type: "Print",
+    description: "Geometry print used processing",
+    websiteLink: "",
+  },
+  {
+    name: "Shibuya card",
+    path: "Shibuya card",
+    coverImg: shibuCard,
+    type: "Print",
+    description: "A visit card with the shibuya logo I made",
+    websiteLink: "",
+  },
+      {
+    name: "etude house",
+    path: "etude house",
+    coverImg: etudePhone,
+    type: "UI design",
+    description: "etude house mobile checkout",
+    websiteLink: "",
+  },
   {
     name: "Cl presave",
     path: "Cl presave",
@@ -28,6 +72,13 @@ let archivesData = [
     type: "Ui design",
     description: "Cl Presave page",
     websiteLink: "",
+  },
+  {
+    name: "Akali giants",
+    path: "Akali giants",
+    coverImg: akali,
+    type: "Ui design",
+    description: "Akali giants ui design",
   },
   {
     name: "Cl Print",
@@ -38,12 +89,27 @@ let archivesData = [
     websiteLink: "",
   },
   {
-    name: "Shibuya Print Mockup",
-    path: "Shibuya Print Mockup",
-    coverImg: shibuya_print,
-    type: "Print",
-    description: "Print for Shibuya",
-    websiteLink: "download",
+    name: "Kaonashi human",
+    path: "Kaonashi human",
+    coverImg: illu1,
+    type: "Illustration",
+    description: "remake from suzani",
+    websiteLink: "",
+  },
+  {
+    name: "vans",
+    path: "vans",
+    coverImg: vans,
+    type: "Ui design",
+    description: "log in page design for vans",
+    websiteLink: "",
+  },
+  {
+    name: "Katy_v4 mini folio",
+    path: "Katy_v4 mini folio",
+    coverImg: katy_v4,
+    type: "website",
+    description: "old website for my v4 folio",
   },
   {
     name: "Shibuya Device Mockup",
@@ -94,22 +160,6 @@ let archivesData = [
     websiteLink: "https://abidaisy.com/",
   },
   {
-    name: "DesignR",
-    path: "DesignR",
-    coverImg: designr,
-    type: "website",
-    description: "An API for designR (work only on desktop)",
-    websiteLink: "https://designr.cathydolle.com/",
-  },
-  {
-    name: "Etude house",
-    path: "Etude house",
-    coverImg: etude,
-    type: "Ui Design",
-    description: "UI Design for Étude House",
-    websiteLink: "",
-  },
-  {
     name: "Nike homepage",
     path: "Nike homepage",
     coverImg: nike,
@@ -125,22 +175,7 @@ let archivesData = [
     description: "remake from suzani",
     websiteLink: "",
   },
-  {
-    name: "Kaonashi human",
-    path: "Kaonashi human",
-    coverImg: illu1,
-    type: "Illustration",
-    description: "remake from suzani",
-    websiteLink: "",
-  },
-  {
-    name: "vans",
-    path: "vans",
-    coverImg: vans,
-    type: "Ui design",
-    description: "log in page design for vans",
-    websiteLink: "",
-  },
+
   {
     name: "Aker",
     path: "Aker",
